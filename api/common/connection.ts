5f954b4a154f8b210ea19874a7bc940acfb81c6c
jpbogle <jpbogle22@gmail.com>
2022-05-27 17:17:35 -0700

    New access token

--- api/common/connection.ts
+++ api/common/connection.ts
@@ -4,12 +4,12 @@ const networkURLs: { [key: string]: { primary: string; secondary?: string } } =
   {
     ["mainnet-beta"]: {
       primary:
-        "https://solana-api.syndica.io/access-token/sAVBWmmS5tiEVca9BZki5DKRGFssefsicszEOE4uGG3vDVr4mOZwMw83jpcMbhz2/rpc",
+        "https://solana-api.syndica.io/access-token/sKMSJpn8HSu4bmAiWoDsxbFyuspVl872uSEWZjFq41K5oOO59JuGHaBfkZNcWNAf/rpc",
       secondary: "https://ssc-dao.genesysgo.net/",
     },
     mainnet: {
       primary:
-        "https://solana-api.syndica.io/access-token/sAVBWmmS5tiEVca9BZki5DKRGFssefsicszEOE4uGG3vDVr4mOZwMw83jpcMbhz2/rpc",
+        "https://solana-api.syndica.io/access-token/sKMSJpn8HSu4bmAiWoDsxbFyuspVl872uSEWZjFq41K5oOO59JuGHaBfkZNcWNAf/rpc",
       secondary: "https://ssc-dao.genesysgo.net/",
     },
     devnet: { primary: "https://api.devnet.solana.com/" },
