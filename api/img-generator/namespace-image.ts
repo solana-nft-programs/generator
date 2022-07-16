9e0f0fe113c545f2fa614e75190841e51e994ea0
jpbogle <jpbogle22@gmail.com>
2022-07-16 04:10:12 -0400

    Cleanup img generator, add strata

---
+++ api/img-generator/namespace-image.ts
@@ -0,0 +1,71 @@
+import { breakIdentity, formatName } from "@cardinal/namespaces";
+import * as canvas from "canvas";
+
+import type { TokenData } from "../common/tokenData";
+import {
+  drawBackgroundImage,
+  drawDefaultBackground,
+  drawText,
+} from "./img-utils";
+
+const WIDTH = 500;
+const HEIGHT = 500;
+
+const IGNORE_TEXT = ["empiredao-registration", "EmpireDAO"];
+// const IDENTITY_COLORS: { [key: string]: string } = {
+//   twitter: "#1DA1F2",
+//   discord: "#5866f2",
+// };
+
+export async function getNamespaceImage(
+  tokenData: TokenData,
+  nameParam: string | undefined,
+  textParam: string | undefined
+) {
+  const mintName = tokenData?.metaplexData?.parsed.data.name;
+  const namespace =
+    (nameParam && !tokenData?.metaplexData?.parsed.data.name?.includes(".")
+      ? tokenData?.metaplexData?.parsed.data.name
+      : breakIdentity(mintName || textParam || "")[0]) || "";
+  const entryName = nameParam
+    ? nameParam
+    : breakIdentity(mintName || textParam || "")[1];
+
+  console.log(`Drawing namespace image for [${namespace}, ${entryName}]`);
+  const imageCanvas = canvas.createCanvas(WIDTH, HEIGHT);
+
+  try {
+    await drawBackgroundImage(
+      imageCanvas,
+      __dirname.concat(`/assets/namespaces/${namespace}.jpg`)
+    );
+  } catch (e) {
+    console.log("Failed to draw background image: ", e);
+    drawDefaultBackground(imageCanvas);
+  }
+
+  let topRightText: string | undefined;
+  let nameText = decodeURIComponent(formatName(namespace, entryName));
+  if (namespace === "discord") {
+    const temp = nameText.split("#");
+    nameText = temp.slice(0, -1).join();
+    topRightText = temp.pop();
+  }
+
+  if (!IGNORE_TEXT.includes(namespace || "")) {
+    drawText(
+      imageCanvas,
+      decodeURIComponent(formatName(namespace || "", entryName))
+    );
+  }
+
+  if (topRightText) {
+    drawText(imageCanvas, topRightText, {});
+    const topRightCtx = imageCanvas.getContext("2d");
+    topRightCtx.font = `${0.08 * WIDTH}px SFPro`;
+    topRightCtx.fillStyle = "white";
+    topRightCtx.textAlign = "right";
+    topRightCtx.fillText("#" + topRightText, WIDTH * 0.95, HEIGHT * 0.1);
+  }
+  return imageCanvas.toBuffer("image/png");
+}
